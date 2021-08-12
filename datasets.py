ac1efc01e2849ce128cc83b2124cdd39bd62ffd2
Pablo Huijse <pablo.huijse@gmail.com>
2021-08-11 20:26:18 -0400

    logmel estático implementado, padding con zeros en collate para todos los features, abstracciones en datasets

--- datasets.py
+++ datasets.py
@@ -1,14 +1,14 @@
-from os.path import join
-from typing import List, Tuple
-
+from os.path import join, isfile, splitext
+from typing import Dict, List, Tuple
+import warnings
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import torch
-from torch.utils.data import Dataset, ConcatDataset, DataLoader
+from torch.utils.data import Dataset, ConcatDataset
 import torchaudio
-from transforms import StereoToMono
 
-datasets_path = "/datasets"
+
+datasets_path = "."
 
 def get_label_transforms(dataset_name):
     a = pd.read_json("fusa_taxonomy.json").T[dataset_name].to_dict()
@@ -18,31 +18,46 @@ def get_label_transforms(dataset_name):
             transforms[value] = key
     return transforms
 
-class ESC(Dataset):
+class ExternalDataset(Dataset):
+
+    def __getitem__(self, idx: int) -> Tuple[str, int]:
+        return (self._file_path(idx), self.labels[idx])
+        
+    def __len__(self) -> int:        
+        return len(self.file_list)
+
+class ESC(ExternalDataset):
 
     def __init__(self):
-        df = pd.read_csv(join(datasets_path, "ESC-50/meta/esc50.csv"))
+        
+        # TODO: Abstract parts of init
         label_transforms = get_label_transforms("ESC")
+        df = pd.read_csv(join(datasets_path, "ESC-50/meta/esc50.csv"))
+        ESC_classes = df["category"].unique()
+        # Verify that there are no typos in FUSA_taxonomy
+        if not all([key in set(ESC_classes) for key in label_transforms.keys() if key != ""]):
+            warnings.warn("Existen llaves de ESC que no calzan en fusa_taxonomy.json", UserWarning)
+        
         self.audio_path = join(datasets_path, "ESC-50/audio")
-        self.file_list = []
-        self.labels = []
-        self.categories = []
-        for label in df["category"].unique():
+        # Verify that files exist
+        file_exist = df["filename"].apply(lambda x: isfile(join(join(datasets_path, "ESC-50/audio"), x)))
+        if not file_exist.all():
+            warnings.warn("Existen rutas incorrectas o archivos perdidos", UserWarning)
+            df = df.loc[file_exist]
+        
+        self.file_list, self.labels, self.categories = [], [], []
+        for label in ESC_classes:
             if label in label_transforms:
                 self.categories += [label_transforms[label]]
                 mask = df.category == label
                 self.file_list += list(df["filename"].loc[mask])
                 self.labels += [label_transforms[label]]*sum(mask)
 
+    def _file_path(self, idx: int) -> str:
+        return join(self.audio_path, self.file_list[idx])
 
-    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, int]:
-        waveform, sample_rate = torchaudio.load(join(self.audio_path, self.file_list[idx]))
-        return (waveform, self.labels[idx])
-        
-    def __len__(self) -> int:        
-        return len(self.file_list)
 
-class UrbanSound8K(Dataset):
+class UrbanSound8K(ExternalDataset):
     
     def __init__(self):
         df = pd.read_csv(join(datasets_path, "UrbanSound8K/metadata/UrbanSound8K.csv"))
@@ -61,30 +76,45 @@ class UrbanSound8K(Dataset):
                 self.labels += [label_transforms[label]]*sum(mask)
         self.fold_list = ['fold' + str(fold) for fold in self.fold_list]
 
-    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, int]:
-        waveform, sample_rate = torchaudio.load(
-            join(self.audio_path, self.fold_list[idx], self.file_list[idx]))
-        return (waveform, self.labels[idx])
-        
-    def __len__(self) -> int:        
-        return len(self.file_list)
+
+    def _file_path(self, idx: int) -> str:
+        return join(self.audio_path, self.fold_list[idx], self.file_list[idx])
+
 
 class FUSAv1(Dataset):
 
-    def __init__(self, transform=None):
-        self.dataset = ConcatDataset([ESC()])
+    def __init__(self, target_sample_rate: int=44100, overwrite_features: bool=False, waveform_transform=None, return_logmel=True):
+        self.dataset = ConcatDataset([ESC(), UrbanSound8K()])
         self.categories = []
         for d in self.dataset.datasets:
             self.categories += d.categories
         self.categories = sorted(list(set(self.categories)))
         self.le = LabelEncoder().fit(self.categories)
-        self.transform = transform
+        self.waveform_transform = waveform_transform
+        self.target_sample_rate = target_sample_rate
+        self.overwrite_features = overwrite_features
+        self.return_logmel = return_logmel
 
-    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        x, y = self.dataset[idx]
-        if self.transform is not None:
-            x = self.transform(x)
-        return x, torch.from_numpy(self.le.transform([y]))
+    def __getitem__(self, idx: int) -> Dict:
+        file_path, label = self.dataset[idx]
+        waveform, sample_rate = torchaudio.load(file_path)
+        # Force resample
+        waveform = torchaudio.transforms.Resample(sample_rate, self.target_sample_rate)(waveform)
+        if self.waveform_transform is not None:
+            waveform = self.waveform_transform(waveform)
+        sample = {'waveform': waveform, 'label': torch.from_numpy(self.le.transform([label]))}
+        # TODO: Implemente overwrite features
+        if self.return_logmel:
+            logmel_path = splitext(file_path)[0]+"_logmel.pt"
+            if isfile(logmel_path):
+                logmel = torch.load(logmel_path)
+            else:
+                mel_transform = torchaudio.transforms.MelSpectrogram(sample_rate=self.target_sample_rate, n_fft=1024, hop_length=512, n_mels=64)
+                logmel = mel_transform(waveform).log()
+                torch.save(logmel, logmel_path)
+            sample['logmel'] = logmel
+                
+        return sample
 
     def __len__(self) -> int:
         return self.dataset.__len__()
@@ -95,10 +125,17 @@ class FUSAv1(Dataset):
     
 if __name__ == '__main__':
 
-    dataset = FUSAv1(transform=StereoToMono())
-    loader = DataLoader(dataset, shuffle=True, batch_size=5)
-    for x, y in loader:
+    from torch.utils.data import DataLoader
+    from transforms import StereoToMono, Collate_and_transform
+    dataset = FUSAv1(waveform_transform=StereoToMono())
+    my_collate = Collate_and_transform(pad=True)
+    loader = DataLoader(dataset, shuffle=True, batch_size=5, collate_fn=my_collate)
+    for batch in loader:
         break
-    print(x.shape, y.shape)
-    print(dataset.label_int2string(y))
+    print(batch['waveform'].shape)
+    print(batch['logmel'].shape)
+    print(dataset.label_int2string(batch['label']))    
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.imshow(batch['logmel'].detach().numpy()[0, 0])
 
--- transforms.py
+++ transforms.py
@@ -1,5 +1,7 @@
 import torch
 from torch import Tensor
+from torch.nn.functional import pad
+from typing import Dict, List, Optional
 
 class StereoToMono(torch.nn.Module):
     """Convert stereo audio to mono."""
@@ -17,3 +19,41 @@ class StereoToMono(torch.nn.Module):
         if waveform.size()[0] == 1:
             return waveform
         return (torch.mean(waveform, 0)).view(1,-1)
+
+
+class Collate_and_transform:
+    
+    def __init__(self, transforms: List=[], pad: bool=False, crop: bool=False):
+        self.transforms = transforms
+        self.pad = pad
+        # TODO: Implement crop
+        #.crop = crop
+
+    def __call__(self, batch: List[Dict]):
+        transformed_batch = []
+        has_logmel = 'logmel' in batch[0] # If one has it everyone has it
+        if self.pad:
+            audio_lens = [sample['waveform'].size(-1) for sample in batch]  
+            max_len_audio = max(audio_lens)  
+            if has_logmel:
+                logmel_lens = [sample['logmel'].size(-1) for sample in batch]  
+                max_len_logmel = max(logmel_lens)
+        for i, sample in enumerate(batch):
+            if self.pad:
+                sample['waveform'] = pad(sample['waveform'], (0, max_len_audio-audio_lens[i]))
+                if has_logmel:
+                    sample['logmel'] = pad(sample['logmel'], (0, max_len_logmel-logmel_lens[i]))                    
+            #if self.crop:
+            #    sample['waveform'] = sample['waveform'][-1, :min_len]
+            #    if 'log_mel' in sample:
+            #        sample['log_mel'] = sample['log_mel'][-1, :min_len]
+            for transform in self.transforms:
+                sample = transform(sample)
+            transformed_batch.append(sample)
+        mbatch = {}
+        mbatch['waveform'] =  torch.stack([sample['waveform'] for sample in transformed_batch], dim=0)
+        if has_logmel:
+            mbatch['logmel'] =  torch.stack([sample['logmel'] for sample in transformed_batch], dim=0)
+        mbatch['label'] = torch.LongTensor([sample['label'] for sample in transformed_batch])
+        return mbatch
+
