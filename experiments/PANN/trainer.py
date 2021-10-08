8099bc257ca7f71555e613d2c6a5a653657abac9
victorvargass <victorvargassandoval93@gmail.com>
2021-10-08 14:44:12 -0300

    cuda support added in pann model

--- experiments/PANN/trainer.py
+++ experiments/PANN/trainer.py
@@ -16,39 +16,40 @@ def create_dataloaders(dataset, params: Dict):
     valid_loader = DataLoader(valid_subset, batch_size=8, collate_fn=my_collate)    
     return train_loader, valid_loader
 
-def train(loaders: Tuple, params: Dict, model_path: str) -> None:
+def train(loaders: Tuple, params: Dict, model_path: str, cuda: bool) -> None:
     """
     Make more abstract to other models
     """
-    train_loader, valid_loader = loaders 
-
-    # Move data to device
-    device = 'cuda'
+    train_loader, valid_loader = loaders
+    n_classes = len(train_loader.dataset.dataset.categories)
 
     n_train, n_valid = len(train_loader.dataset), len(valid_loader.dataset)    
     model = torch.load(model_path)
-    model.fc_audioset = nn.Linear(2048, 15)
-    #criterion = torch.nn.CrossEntropyLoss(reduction='mean') 
+    model.fc_audioset = nn.Linear(2048, n_classes)
     criterion = torch.nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=params['train']['learning_rate'])
-    #optimizer = torch.optim.Adam(model.parameters(), lr=params['train']['learning_rate'], 
-    #    betas=(0.9, 0.999), eps=1e-08, weight_decay=0., amsgrad=True)
 
-    print('GPU number: {}'.format(torch.cuda.device_count()))
-
-    if 'cuda' in str(device):
-        model.to(device)
+    if cuda and torch.cuda.device_count() > 0:
+        print('GPU number: {}'.format(torch.cuda.device_count()))
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    print(f'Using {device}')
 
     best_valid_loss = np.inf
     for epoch in range(params["train"]["nepochs"]):
         global_loss = 0.0
+        model.to(device)
         model.train()
         for batch in train_loader:
-            waveforms, labels = batch['waveform'].to(device), batch['label']
-            labels = (torch.zeros(len(labels), 15).scatter_(1, labels.unsqueeze(1), 1.)).to(device)
+            marshalled_batch = {}
+            for key in batch:
+                if key == 'label':
+                    batch[key] = torch.zeros(len(batch[key]), n_classes).scatter_(1, batch[key].unsqueeze(1), 1.)
+                marshalled_batch[key] = batch[key].to(device)
             optimizer.zero_grad()
-            y = model.forward(waveforms)['clipwise_output']
-            loss = criterion(y, labels)
+            y = model.forward(marshalled_batch['waveform'])['clipwise_output']
+            loss = criterion(y, marshalled_batch['label'])
             loss.backward()
             optimizer.step()
             global_loss += loss.item()
@@ -59,24 +60,26 @@ def train(loaders: Tuple, params: Dict, model_path: str) -> None:
         model.eval()
         with torch.no_grad():
             for batch in valid_loader:
-                waveforms, labels = batch['waveform'].to(device), batch['label']
-                labels = (torch.zeros(len(labels), 15).scatter_(1, labels.unsqueeze(1), 1.)).to(device)
-                y = model.forward(waveforms)['clipwise_output']
-                loss = criterion(y, labels)
+                marshalled_batch = {}
+                for key in batch:
+                    if key == 'label':
+                        batch[key] = torch.zeros(len(batch[key]), n_classes).scatter_(1, batch[key].unsqueeze(1), 1.)
+                    marshalled_batch[key] = batch[key].to(device)
+                y = model.forward(marshalled_batch['waveform'])['clipwise_output']
+                loss = criterion(y, marshalled_batch['label'])
                 global_loss += loss.item()            
         print(f"{epoch}, valid/loss {global_loss/n_valid:0.4f}")
         dvclive.log('valid/loss', global_loss/n_valid)
         dvclive.next_step()
 
-        if global_loss < best_valid_loss: 
+        if global_loss < best_valid_loss:
+            if device == 'cuda': model.cpu()
             torch.save(model, model_path)
             model.create_trace()
 
 def evaluate_model(loaders: Tuple, params: Dict, model_path: str) -> None:
     train_loader, valid_loader = loaders 
     model = torch.load(model_path)
-    model = model.cpu()
-    model.fc_audioset = nn.Linear(2048, 15)
     model.eval()
     
     preds = []
