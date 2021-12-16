88ebfdc35e1a09d70ca66994562df07629486e75
victorvargass <victorvargassandoval93@gmail.com>
2021-12-16 15:08:41 -0300

    time diff on training epochs added and num workers setted

--- experiments/PANN/trainer.py
+++ experiments/PANN/trainer.py
@@ -8,6 +8,7 @@ from dvclive import Live
 from fusanet_utils.transforms import Collate_and_transform
 import torch.nn as nn
 import pandas as pd
+import time
 
 live = Live()
 logger = logging.getLogger(__name__)
@@ -17,8 +18,8 @@ def create_dataloaders(dataset, params: Dict):
     valid_size = len(dataset) - train_size
     train_subset, valid_subset = random_split(dataset, (train_size, valid_size), generator=torch.Generator().manual_seed(params["train"]["random_seed"]))
     my_collate = Collate_and_transform(params['features'])
-    train_loader = DataLoader(train_subset, shuffle=True, batch_size=params["train"]["batch_size"], collate_fn=my_collate)
-    valid_loader = DataLoader(valid_subset, batch_size=8, collate_fn=my_collate)
+    train_loader = DataLoader(train_subset, shuffle=True, batch_size=params["train"]["batch_size"], collate_fn=my_collate, num_workers=4, pin_memory=True)
+    valid_loader = DataLoader(valid_subset, batch_size=8, collate_fn=my_collate, num_workers=4, pin_memory=True)
     return train_loader, valid_loader
 
 def train(loaders: Tuple, params: Dict, model_path: str, cuda: bool) -> None:
@@ -37,22 +38,25 @@ def train(loaders: Tuple, params: Dict, model_path: str, cuda: bool) -> None:
     if cuda and torch.cuda.device_count() > 0:
         logger.info('GPU number: {}'.format(torch.cuda.device_count()))
         device = 'cuda'
+        torch.backends.cudnn.benchmark = True
     else:
         device = 'cpu'
-    logger.info(f'Using {device}')  
+    logger.info(f'Using {device}') 
+
+    model.to(device)
 
     best_valid_loss = np.inf
     for epoch in range(params["train"]["nepochs"]):
         global_loss = 0.0
         global_accuracy = 0.0  
-        model.to(device)
         model.train()
+        start_time = time.time()
         for batch in train_loader:
             marshalled_batch = {}
             for key in batch:
                 if key == 'label':
                     batch[key] = torch.zeros(len(batch[key]), n_classes).scatter_(1, batch[key].unsqueeze(1), 1.)
-                marshalled_batch[key] = batch[key].to(device)
+                marshalled_batch[key] = batch[key].to(device, non_blocking=True)
             optimizer.zero_grad()
             y = model.forward(marshalled_batch['waveform'])['clipwise_output']
             loss = criterion(y, marshalled_batch['label'])
@@ -65,18 +69,20 @@ def train(loaders: Tuple, params: Dict, model_path: str, cuda: bool) -> None:
         logger.info(f"{epoch}, train/accuracy {global_accuracy/n_train:0.4f}")
         live.log('train/loss', global_loss/n_train)
         live.log('train/accuracy', global_accuracy/n_train)
+        logger.info(f"train time, {time.time() - start_time}")
         
         global_loss = 0.0
         global_accuracy = 0.0 
         global_f1_score = 0.0 
         model.eval()
+        start_time = time.time()
         with torch.no_grad():
             for n_batch, batch in enumerate(valid_loader, start=1):
                 marshalled_batch = {}
                 for key in batch:
                     if key == 'label':
                         batch[key] = torch.zeros(len(batch[key]), n_classes).scatter_(1, batch[key].unsqueeze(1), 1.)
-                    marshalled_batch[key] = batch[key].to(device)
+                    marshalled_batch[key] = batch[key].to(device, non_blocking=True)
                 y = model.forward(marshalled_batch['waveform'])['clipwise_output']
                 loss = criterion(y, marshalled_batch['label'])
                 global_loss += loss.item()            
@@ -89,6 +95,7 @@ def train(loaders: Tuple, params: Dict, model_path: str, cuda: bool) -> None:
         live.log('valid/loss', global_loss/n_valid)
         live.log('valid/accuracy', global_accuracy/n_valid)
         live.log('f1_score macro', global_f1_score/n_batch)
+        logger.info(f"valid time, {time.time() - start_time}")
         live.next_step()
 
         if global_loss < best_valid_loss:
