fdfbd8c23625b3187f6ae1a7e9d5f3310b3f0825
Pablo Huijse <pablo.huijse@gmail.com>
2022-12-07 00:00:06 -0300

    experiment manager refactored

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -3,7 +3,10 @@ import yaml
 import logging
 import json
 import argparse
-import fusanet_utils.experiment_manager as manager
+from fusanet_utils.experiments.initializer import initialize_model, create_dataset, create_dataloaders
+from fusanet_utils.experiments.trainer import train_model
+from fusanet_utils.experiments.evaluator import evaluate_model
+
 
 def dir_path(path):
     if os.path.exists(path):
@@ -15,14 +18,18 @@ def dir_path(path):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--root_path', dest='root_path', help='path to the root of this repo', type=dir_path, default='../')
-    parser.add_argument('--model_path', dest='model_path', help='path to save/load model', type=str, default='model.pt')
+    parser.add_argument('--root_path', dest='root_path',
+                        help='path to the root of this repo',
+                        type=dir_path, default='../')
+    parser.add_argument('--model_path', dest='model_path',
+                        help='path to save/load model',
+                        type=str, default='model.pt')
     parser.add_argument('--train', action='store_true')
     parser.add_argument('--evaluate', action='store_true')
-    parser.add_argument('--pretrained', action='store_true')
-    parser.add_argument('--cuda', action='store_true')
-    parser.add_argument('--verbose', help="Print info level logs", action="store_true")
-    parser.add_argument('--debug', help="Print debug level logs", action="store_true")
+    parser.add_argument('--verbose', help="Print info level logs",
+                        action="store_true")
+    parser.add_argument('--debug', help="Print debug level logs",
+                        action="store_true")
     args = parser.parse_args()
 
     # Logging
@@ -42,18 +49,18 @@ if __name__ == "__main__":
     main_logger.info("Loading parameters, dataset and model")
     params = yaml.safe_load(open("params.yaml"))
     if args.train:
-        dataset = manager.create_dataset(args.root_path, params, stage='train')
+        dataset = create_dataset(args.root_path, params, stage='train')
         with open('index_to_name.json', 'w') as f:
             json.dump(dataset.label_dictionary(), f)
         main_logger.info("Creating dataloaders")
-        loaders = manager.create_dataloaders(dataset, params)
-        # Save initial model
-        manager.initialize_model(args.model_path, params['train'], len(dataset.categories), args.cuda, args.pretrained)
+        loaders = create_dataloaders(dataset, params)
+        initialize_model(args.model_path, params['train'],
+                         len(dataset.categories))
         main_logger.info("Main: Training")
-        manager.train(loaders, params, args.model_path, args.cuda)
+        train_model(loaders, params, args.model_path)
     if args.evaluate:
         main_logger.info(f"Main: Evaluating on {params['evaluate']['dataset']}")
-        dataset = manager.create_dataset(args.root_path, params, stage='evaluate')
+        dataset = create_dataset(args.root_path, params, stage='evaluate')
         with open('index_to_name.json', 'r') as f:
             label_dictionary = json.load(f)
-        manager.evaluate_model(dataset, params, args.model_path, label_dictionary)
+        evaluate_model(dataset, params, args.model_path, label_dictionary)
